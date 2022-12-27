7688d0a497f9de21f0f6705c8e454e96ce4f8698
AaronGramajo <Aarongramajo85@gmail.com>
2022-12-27 11:22:13 -0300

    cambie logger en infoRoutes

--- Proyecto/routes/infoRoutes.js
+++ Proyecto/routes/infoRoutes.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const compression = require('compression')
+const logger = require('../utils/log4js.js')
 
 const infoRouter = Router()
 
@@ -7,7 +8,7 @@ infoRouter.get('/', async (req, res) => {
     try {
         res.status(200).render('pages/info.ejs')
     } catch (error) {
-        res.status(404).json({message: `${error}`})
+        res.status(404).send(logger.error(`${error}`))
     }
 })
 
@@ -15,7 +16,7 @@ infoRouter.get('/zip', compression(), async (req, res) => {
     try {
         res.status(200).render('pages/info.ejs')
     } catch (error) {
-        res.status(404).json({message: `${error}`})
+        res.status(404).send(logger.error(`${error}`))
     }
 })
 
